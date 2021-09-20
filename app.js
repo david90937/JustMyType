8a1ad7bee94a3fd7739fa2a1142b7871910d1201
David Robinette <drobin4@gmail.com>
2021-09-19 22:18:21 -0500

    fixed left over characters when sentence change / game reset. Added comments. Other minor changes.

--- app.js
+++ app.js
@@ -8,34 +8,18 @@ $(document).ready(function () {
     let numberOfWords = 0;
     let numberofErrors = 0;
     let key;
-
+    
+    // sets upper case keyboard to be hidden when game starts
     $('#keyboard-upper-container').children('div').each(function () {
         $(this).toggle();
     })
     $(document).keydown(function (event) {
         key = event.which;
         $('#' + event.which).css('background', 'yellow');
-        if (counter == 48){
-            $('#feedback').empty();
-            //numberOfWords++
-            let endTime = performance.now();
-            let playTime = Math.round((endTime - startTime) / 1000);
-            let wordsPerMinute = (numberOfWords/(playTime/60)) - (2 * numberofErrors);
-            $('#feedback').append('<span>' + wordsPerMinute + '</span>');
-            counter++
-            resetGame();
-        }     
-        if (counter == (sentences[currentSentence].length -1 ) && (currentSentence <= 3)){
-            currentSentence++;
-            numberOfWords++; // this is here to account for the last word of each sentence
-            counter = 0;
-            $('#sentence').empty();
-            $('#feedback').empty();
-            changeSentence();   
-        }
         if (event.which >= 65 && event.which <= 90) {
             $('#' + (event.which + 32)).css('background', 'yellow');
         }
+        // if shift is pressed toggles keyboard display on key down
         if (keyPressed === false) {
             keyPressed = true;
             if (key == 16) {
@@ -43,25 +27,46 @@ $(document).ready(function () {
                 $('#keyboard-lower-container').children('div').toggle();
             }
         }
-        if (event.key == $('#c'+counter)[0].innerText && counter < 48){ // hard coded 48 to prevent type error at end of game
-            $('#c' + (counter+1)).css('background', 'yellow');
+        // prevents wrong key presses from advancing the game. Increments counter on correct key press to continue game. Appends check mark if correct key pressed, cross mark if not.
+        if (event.key == $('#c' + counter)[0].innerText && counter < sentences[currentSentence].length) { // hard coded 48 to prevent type error at end of game
+            $('#c' + (counter + 1)).css('background', 'yellow');
             $('#c' + counter).css('background', '');
             counter++;
             $('#target-letter').empty();
-            $('#target-letter').append($('#c'+counter)[0].innerText)
+            $('#target-letter').append($('#c' + counter)[0].innerText)
             $('#feedback').append('<span> &#x2705 </span>');
         }
         else {
             $('#feedback').append('<span> &#x274c </span>');
             numberofErrors++
         }
+        console.log(counter);
     })
     $(document).keyup(function (event) {
         key = event.which;
         $('#' + event.which).css('background', '');
+        // if at end of string and not last sentence, changes sentence. 
+        if (counter == (sentences[currentSentence].length) && (currentSentence <= (sentences.length - 2))) {
+            currentSentence++;
+            numberOfWords++; // this is here to account for the last word of each sentence
+            counter = 0;
+            changeSentence();
+        }
+        // if at end of last sentence - empty fields, display result, and reset game. 
+        if (counter == (sentences[currentSentence].length) && currentSentence == (sentences.length - 1)) {
+            $('#feedback').empty();
+            $('#sentence').empty();
+            //numberOfWords++
+            displayResult();
+            setTimeout(_ => {
+                resetGame();
+            }, 2000)
+        }
+        // .which returns the same code for both upper and lower case letters. This adds 32 to ensure all lower case letters has highlight removed properly. No support for non-alpha numeric numbers are this time.
         if (event.which >= 65 && event.which <= 90) {
             $('#' + (event.which + 32)).css('background', '');
         }
+        // if shift is pressed toggles keyboards on key release
         if (key == 16) {
             if (keyPressed === true) {
                 keyPressed = false;
@@ -71,33 +76,43 @@ $(document).ready(function () {
         }
         keyPressed = false;
     })
-
-    function changeSentence (){
+    // Clears previous feedback/sentence and changes sentence when called. 
+    function changeSentence() {
         sentenceChars = [];
+        $('#feedback').empty();
+        $('#sentence').empty();
+        // loops through provided sentence array and its indexes to push each string character into a new array. 
         for (let x = 0; x < sentences[currentSentence].length; x++) {
-             sentenceChars.push(sentences[currentSentence][x]);
-             // counts the number of spaces to determine how many words are in a given string
-             if (sentences[currentSentence][x] == " "){
-                numberOfWords++ 
-             } 
+            sentenceChars.push(sentences[currentSentence][x]);
+            // counts the number of spaces to determine how many words are in a given string
+            if (sentences[currentSentence][x] == " ") {
+                numberOfWords++
+            }
         }
+        // loops through current index of sentences and appends the characters one at a time
         for (let y = 0; y < sentences[currentSentence].length; y++) {
             $('#sentence').append('<span id =c' + y + '>' + sentenceChars[y] + '</span>');
         }
     }
-    
-    function resetGame(){
+    // Resets game when called
+    function resetGame() {
         let userChoice = confirm("Play again?")
-        if (userChoice){
+        if (userChoice) {
             currentSentence = 0;
             numberOfWords = 0; // this is here to account for the last word of each sentence
             counter = 0;
             startTime = 0;
             numberofErrors = 0;
-            $('#sentence').empty();
-            $('#feedback').empty();
             changeSentence();
         }
+    }
+    // Calculates and displays score when called
+    function displayResult() {
+        $('#feedback').empty();
+        let endTime = performance.now();
+        let playTime = Math.round((endTime - startTime) / 1000);
+        let wordsPerMinute = Math.round((numberOfWords / (playTime / 60)) - (2 * numberofErrors));
+        $('#feedback').append('<span> Score: ' + wordsPerMinute + '</span>');
     }
     changeSentence();
 })
